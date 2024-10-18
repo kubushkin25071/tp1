901673be073a8215f90ff9f54a008bd31492f83b
kubushkin25071 <kubushkin2507@gmail.com>
2024-10-18 18:55:06 +0300

    Menu and keeper's methods

--- keeper.h
+++ keeper.h
@@ -13,8 +13,17 @@ private:
 	vector<String*> Strings;
 	vector<Brass*> Brasses;
 public:
-	Keeper();
-	~Keeper();
-	
+	Keeper();	// конструктор по-умолчанию
+	~Keeper();	// деструктор
+
+	Read_file(string file_name);
+	Write_file(string file_name);
+
+	Add_instrument(string toi, string name, int cost, string fio, string tp1, string tp2);		//tp1 и tp2 - поля, которые имеют различное назначение у параметров
+	Delete_instrument(string toi, int index);
+
+	vector<Drum*> GetDrums();
+	vector<String*> GetStrings();
+	vector<Brass*> GetBrasses();
 };
 
--- main.cpp
+++ main.cpp
@@ -2,9 +2,17 @@
 #include "keeper.h"
 
 int main(){
+	char c = ' ';
 	setlocale(LC_ALL, "Russian");
 	system("cls");
-	cout << "Начать работу - 1" << endl;
-	cout << "Выйти - 0" << endl;
+	while (true){
+		cout << "Начать работу - 1" << endl;
+		cout << "Выйти - 0" << endl;
+		cin >> c;
+		if (c == '1') system("cls");// идёт остальное меню
+		else if (c == '0')	break;
+		else cout << "Введён некорректный символ" << endl;
+	}
+	
 	return 0;
 }
